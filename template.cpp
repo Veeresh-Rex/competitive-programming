d9f542e21fbd5b6d526c96e76c2320c8194b1590
Veeresh Rex <52687597+Veeresh-Rex@users.noreply.github.com>
2020-08-19 19:55:16 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -43,7 +43,11 @@ int32_t main()
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-   
-
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        /* code */
+    }
     return 0;
 }
