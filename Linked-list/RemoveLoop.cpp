45ff2b4a14181e74ba5642a2d5bb431d85ed3e90
Veeresh Rex <52687597+Veeresh-Rex@users.noreply.github.com>
2020-08-27 14:25:08 +0530

    Create RemoveLoop.cpp

---
+++ Linked-list/RemoveLoop.cpp
@@ -0,0 +1,162 @@
+/* Input:
+N = 3
+value[] = {1,3,4}
+C = 2
+Output: 1
+Explanation: In the first test case
+N = 3.The linked list with nodes
+N = 3 is given. Here, x = 2 which
+means last node is connected with xth
+node of linked list. Therefore, there
+exists a loop. 
+
+
+*/
+// driver code
+
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* next;
+    
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+void loopHere(Node* head, Node* tail, int position)
+{
+    if(position==0) return;
+    
+    Node* walk = head;
+    for(int i=1; i<position; i++)
+        walk = walk->next;
+    tail->next = walk;
+}
+
+bool isLoop(Node* head)
+{
+    if(!head) return false;
+    
+    Node* fast = head->next;
+    Node* slow = head;
+    
+    while( fast != slow)
+    {
+        if( !fast || !fast->next ) return false;
+        fast=fast->next->next;
+        slow=slow->next;
+    }
+    
+    return true;
+}
+
+int length(Node* head)
+{
+    int ret = 0;
+    while(head)
+    {
+        ret++;
+        head = head->next;
+    }
+    return ret;
+}
+
+void removeLoop(Node* head);
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n, num;
+        cin>>n;
+        
+        Node *head, *tail;
+        cin>> num;
+        head = tail = new Node(num);
+        
+        for(int i=0 ; i<n-1 ; i++)
+        {
+            cin>> num;
+            tail->next = new Node(num);
+            tail = tail->next;
+        }
+        
+        int pos;
+        cin>> pos;
+        loopHere(head,tail,pos);
+        
+        removeLoop(head);
+        
+        if( isLoop(head) || length(head)!=n )
+            cout<<"0\n";
+        else
+            cout<<"1\n";
+    }
+	return 0;
+}
+// } Driver Code Ends
+
+
+/*
+structure of linked list node:
+
+struct Node
+{
+    int data;
+    Node* next;
+    
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+*/
+
+
+void removeLoop(Node* head)
+{
+    // code here
+    // just remove the loop without losing any nodes
+    Node *slow=head;
+    Node *fast=head;
+    while(slow&&fast&&fast->next)
+    {
+        slow=slow->next;
+        fast=fast->next->next;
+        if(slow==fast)
+        {
+            int k=1;
+            slow=slow->next;
+            while(slow!=fast)
+            {
+                k++;
+                slow=slow->next;
+            }
+            slow=head;
+            fast=head;
+            for(int i=0;i<k;i++)
+              fast=fast->next;
+            while(fast!=slow)
+            {
+                slow=slow->next;
+                fast=fast->next;
+            }
+            while(fast->next!=slow)
+            {
+                fast=fast->next;
+            }
+            fast->next=NULL;
+        
+        }
+    }
+}
