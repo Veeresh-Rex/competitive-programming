13ba11f3fc31e2e80897cfc0005eec30b0d9b648
Veeresh Rex <52687597+Veeresh-Rex@users.noreply.github.com>
2020-09-06 08:51:55 +0530

    Create minCoinChange.cpp

---
+++ Greedy/minCoinChange.cpp
@@ -0,0 +1,77 @@
+//ON indian currency
+#include <bits/stdc++.h>
+using namespace std;
+#define gc getchar_unlocked
+#define fo(i, n) for (i = 0; i < n; i++)
+#define Fo(i, k, n) for (i = k; k < n ? i < n : i > n; k < n ? i += 1 : i -= 1)
+#define ll long long
+#define si(x) scanf("%d", &x)
+#define sl(x) scanf("%lld", &x)
+#define ss(s) scanf("%s", s)
+#define pi(x) printf("%d\n", x)
+#define pl(x) printf("%lld\n", x)
+#define ps(s) printf("%s\n", s)
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, a) for (auto it = a.begin(); it != a.end(); it++)
+#define PI 3.1415926535897932384626
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+int mpow(int base, int exp);
+void ipgraph(int m);
+void dfs(int u, int par);
+int mincoin(int n)
+{
+    vi arr = {1, 2, 5, 10, 20, 50, 100, 200, 500, 2000};
+    auto low = lower_bound(arr.begin(), arr.end(), n);
+    if (*low == n)
+        return *low;
+    return *(low - 1);
+}
+vi solve(int n)
+{
+    vi coins;
+    int k;
+    while (n != 0)
+    {
+        k = mincoin(n);
+        coins.pb(k);
+        n -= k;
+    }
+    return coins;
+}
+int32_t main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vi coins = solve(n);
+        for (auto i : coins)
+            cout << i << " ";
+        cout << "\n";
+    }
+    return 0;
+}
