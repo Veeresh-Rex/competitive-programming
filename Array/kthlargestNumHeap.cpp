ecfb43365f83242c248f46abbfed1c29396286b8
Veeresh Rex <52687597+Veeresh-Rex@users.noreply.github.com>
2020-09-17 17:21:16 +0530

    Create kthlargestNumHeap.cpp

---
+++ Array/kthlargestNumHeap.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define gc getchar_unlocked
+#define fo(i, n) for (i = 0; i < n; i++)
+#define Fo(i, k, n) for (i = k; k < n ? i < n : i > n; k < n ? i += 1 : i -= 1)
+#define ll long long
+#define si(x) scanf("%d", &x)
+#define sl(x) scanf("%lld", &x)
+#define ss(s) scanf("%s", s)
+#define pi(x) printf("%d\n", x)
+#define pl(x) printf("%lld\n", x)
+#define ps(s) printf("%s\n", s)
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, a) for (auto it = a.begin(); it != a.end(); it++)
+#define PI 3.1415926535897932384626
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+int mpow(int base, int exp);
+void ipgraph(int m);
+void dfs(int u, int par);
+int solve(vi arr, int k)
+{
+    priority_queue<int, vi, greater<int>> pq;
+    int i = 0;
+    for (i = 0; i < k; i++)
+    {
+        pq.push(arr[i]);
+    }
+    while (i != arr.size())
+    {
+        if (pq.top() < arr[i])
+        {
+            pq.pop();
+            pq.push(arr[i]);
+        }
+        i++;
+    }
+    return pq.top();
+}
+int32_t main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int k;
+        cin >> k;
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+        cout << solve(arr, k);
+    }
+    return 0;
+}
