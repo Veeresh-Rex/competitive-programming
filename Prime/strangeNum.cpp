a571fb3293731b6e7e6d6b0b6fb4fb016f9f53ac
Veeresh Rex <mauryaveeresh@gmail.com>
2020-12-14 23:01:37 +0530

    Create strangeNum.cpp

---
+++ Prime/strangeNum.cpp
@@ -0,0 +1,69 @@
+// program statement - https://www.codechef.com/problems/STRNO/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define gc getchar_unlocked
+#define fo(i, n) for (i = 0; i < n; i++)
+#define Fo(i, k, n) for (i = k; k < n ? i < n : i > n; k < n ? i += 1 : i -= 1)
+#define ll long long
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, a) for (auto it = a.begin(); it != a.end(); it++)
+#define PI 3.1415926535897932384626
+#define mod 1000000007
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, k;
+        cin >> n >> k;
+        vector<int> alldivisers;
+        while (n % 2 == 0)
+        {
+            alldivisers.push_back(2);
+            n /= 2;
+        }
+        for (int i = 3; i * i <= n; i += 2)
+        {
+            while (n % i == 0)
+            {
+                alldivisers.pb(i);
+                n /= i;
+            }
+        }
+        if (n > 2)
+            alldivisers.pb(n);
+        int c = alldivisers.size();
+        if (c >= k)
+            cout << 1 << "\n";
+        else
+        {
+            cout << 0 << "\n";
+        }
+    }
+    return 0;
+}
